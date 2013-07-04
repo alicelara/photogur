35b68e21769fb60d4dcb40b05987491bacea8efd
Alice Lara <hello@alicelara.ca>
2013-07-04 14:19:52 -0400

    adding picture to the controller and index files

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,7 +1,23 @@
 class PicturesController < ApplicationController
 
 	def index
-
+		@pictures = [
+			{
+				:title => "The olde church...",
+				:artist => "Sergey Ershov",
+				:url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+			},
+			{
+				:title => "Sea Power",
+				:artist => "Stephen Scullion",
+				:url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
+			},
+			{
+				:title => "Into the Poppies",
+				:artist => "John Wilhelm",
+				:url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
+			}
+		]
 	end
 
 end
