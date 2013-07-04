fa78960fbb1b9b2f4a65c9cfee8e099b009d9417
Alice Lara <hello@alicelara.ca>
2013-07-04 17:57:36 -0400

    Moving @pictures in pictures_controller.rb into database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Picture.create!(
+    :title  => "Carambola",
+    :artist => "Beatriz Milhazes",
+    :url    => "http://uploads1.wikipaintings.org/images/beatriz-milhazes/carambola-2008.jpg"
+  )
+
+Picture.create!(
+    :title  => "gatos",
+    :artist => "Romero Britto",
+    :url    => "http://static.tumblr.com/b8rmj05/NbQm3rlhi/romero-britto.jpg"  
+  )
+
+Picture.create!(
+    :title  => "Boston",
+    :artist => "Os Gemeos",
+    :url    => "http://25.media.tumblr.com/tumblr_mags7kVdXH1rweul4o1_r1_1280.jpg"  
+  )
